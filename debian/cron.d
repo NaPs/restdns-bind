099e10f065466f09400fe40d691f254940ef5d7c
Antoine Millet <antoine@inaps.org>
2013-07-21 20:44:24 +0200

    Added Debian packaging

---
+++ debian/cron.d
@@ -0,0 +1,6 @@
+# RestDNS Bind9 client crontab file
+
+# Uncomment the following line to enable restdns-bind:
+#  (don't forget to customize the restdns server address)
+
+#* * * * * bind restdns-bind --syslog http://my-restdns-server/ /etc/bind/restdns/
