359b8ed8f0cc2b7fab7f8e317fce31ca68a4bd32
Antoine Millet <antoine@inaps.org>
2013-07-29 21:27:32 +0200

    Started to work on v1.1~dev

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,9 @@
 RestDNS Bind Changelog
 ======================
 
+v1.1 (not yet released)
+-----------------------
+
 v1.0 released on 21/07/2013
 ---------------------------
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 
-version = '1.0'
+version = '1.1~dev'
 ldesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 
